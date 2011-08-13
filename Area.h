300b117071ec3a0e58414a7f9f2b6ba19f8c1ba4
toby <toby.hster@hotmail.com>
2011-08-13 16:23:58 +1200

    toby is cool

--- Area.h
+++ Area.h
@@ -17,110 +17,26 @@ class Area{
    std::vector<StateDescriptor*> description;
    std::vector<AreaCommand*> commands;
  public:
-   bool has_description(std::string desc_id){
-      for(int desc = 0; desc < num_descriptions; desc++){
-         if(!strcmp(description[desc]->get_id().c_str(), desc_id.c_str())){
-            return true;
-         }
-      }
-      return false;
-   }
-   std::string get_status(){
-      return status;
-   }
-   bool has_current_desc(){
-      return has_description(curr_desc_id);
-   }
-   int get_num_items(){
-      return num_items;
-   }
-   
-   std::string get_description(){
-      for(int desc = 0; desc < num_descriptions; desc++){
-         if(!strcmp(description[desc]->get_id().c_str(),curr_desc_id.c_str() )){
-            return description[desc]->get_description();
-         }
-      }
-      return "";
-   }
-   void remove_item(int index){
-      items.erase(items.begin()+index);
-      num_items--;
-   }
-   void add_item(Item *new_item){
-      items.push_back(new_item);
-      num_items++;
-   }
-   Item *get_item(int index){
-      return items[index];
-   }
-   std::string get_id(){
-      return id;
-   }
-   bool has_item(std::string item_to_find){
-      for(unsigned int item_num = 0; item_num < items.size(); item_num++){
-         if(items[item_num]->get_id().compare(item_to_find) == 0){
-            return true;
-         }
-      }
-      return false;
-   }
-   Item *get_item(std::string item_id){
-      for(unsigned int item_num = 0; item_num < items.size(); item_num++){
-         if(items[item_num]->get_id().compare(item_id) == 0){
-            return items[item_num];
-         }
-      }
-      return NULL;
-   }
-   void add_description(StateDescriptor *desc){
-      description.push_back(desc);
-      num_descriptions++;
-      
-   }
-   void add_command(AreaCommand *command_name){
-      commands.push_back(command_name);
-      num_commands++;
-   }
-   int get_num_commands(){
-      return num_commands;
-   }
-   AreaCommand *get_command(int index){
-      return commands[index];
-   }
-   AreaCommand *has_command(std::string command_name){
-      for(unsigned int c_num = 0; c_num < commands.size(); c_num++){
-         if(!commands[c_num]->get_name().compare(command_name)){
-            return commands[c_num];
-         }
-      }
-      return NULL;
-   }
-   int get_num_descriptions(){
-      return num_descriptions;
-   }
-   StateDescriptor *get_descriptor(int index){
-      return description[index];
-   }
-   Area(const char *id, const char *desc_id, const char* status){
-      this->status = status;
-      this->id = id;
-      num_descriptions = 0;
-      num_items = 0;
-      num_commands = 0;
-      this->curr_desc_id = desc_id;
-   }
-   ~Area(){
-      for(unsigned int item_num = 0; item_num < items.size(); item_num++){
-         delete items[item_num];
-      }
-      for(unsigned int desc_num = 0; desc_num < description.size(); desc_num++){
-         delete description[desc_num];
-      }
-      for(unsigned int command_num = 0; command_num < commands.size(); command_num++){
-         delete commands[command_num];
-      }
-   }
+   bool has_description(std::string desc_id);
+   std::string get_status();
+   bool has_current_desc();
+   int get_num_items();
+   std::string get_description();
+   void remove_item(int index);
+   void add_item(Item *new_item);
+   Item *get_item(int index);
+   std::string get_id();
+   bool has_item(std::string item_to_find);
+   Item *get_item(std::string item_id);
+   void add_description(StateDescriptor *desc);
+   void add_command(AreaCommand *command_name);
+   int get_num_commands();
+   AreaCommand *get_command(int index);
+   AreaCommand *has_command(std::string command_name);
+   int get_num_descriptions();
+   StateDescriptor *get_descriptor(int index);
+   Area(const char *id, const char *desc_id, const char* status);
+   ~Area();
 };
 
 #endif
