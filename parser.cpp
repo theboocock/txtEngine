0e1efcccdfbb3ae5431450efbd37f6bf70b9d913
toby <toby.hster@hotmail.com>
2011-08-14 16:50:33 +1200

    toby

--- parser.cpp
+++ parser.cpp
@@ -46,7 +46,7 @@ ItemCommand *make_item_command(TiXmlNode *pCommand, const char *parent_id, World
    const char *error_tag = MISSING_TAGS, 
       *command_name = INVALID, *command_state = INVALID,
       *command_area = INVALID, *command_status = NONE, *command_depends = NONE,
-      *synonyms = NULL, *unlock = NULL;
+      *synonyms = NULL, *unlock = NONE;
    int attributesFound = 0;
    bool has_name = false, has_state = false, command_chg_col = true, command_dep =true,
       has_collect = false, has_collec_dep = false, has_area = false, has_status = false,
